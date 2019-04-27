07dc10c7779758270d817ad58d0da960bb9b87c4
unknown <kutashov0192@gmail.com>
2019-04-27 14:42:08 +0400

    add layout

--- app/src/main/java/com/GFPCommand/UAFA/FlashActivity.java
+++ app/src/main/java/com/GFPCommand/UAFA/FlashActivity.java
@@ -2,12 +2,24 @@ package com.GFPCommand.UAFA;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.hardware.camera2.*;
+import android.widget.Button;
 
 public class FlashActivity extends AppCompatActivity {
+
+    private Button flashActivate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flash);
+
+        flashActivate = findViewById(R.id.flashAct);
+
+        flashActivate.setOnClickListener(v -> onClick());
+    }
+
+    public void onClick(){
+
     }
 }
